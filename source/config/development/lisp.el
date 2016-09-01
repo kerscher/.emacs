e5d9d6bbf10be0ca4a1f1744b5242a75f759b059
Yghor Kerscher <kerscher@acm.org>
2016-09-01 12:24:18 -0300

    Paredit is not default on lisp buffers anymore.

--- source/config/development/lisp.el
+++ source/config/development/lisp.el
@@ -4,5 +4,6 @@
 (customize-set-variable 'debug-on-error t)
 
 ;; Structured editing
-(require 'paredit)
-(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+;;(require 'paredit)
+;;(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+
