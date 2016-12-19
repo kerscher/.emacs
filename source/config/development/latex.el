651da05d68b86cb6d519541978977bedf1f6d810
Yghor Kerscher <kerscher@acm.org>
2016-12-19 13:21:35 +0100

    LaTeX completions with `company-bibtex`.

--- source/config/development/latex.el
+++ source/config/development/latex.el
@@ -1,8 +1,17 @@
 ;; LaTeX
 
-(setq-default TeX-PDF-mode t)
-
 (require 'auctex-latexmk)
 (auctex-latexmk-setup)
 
+;; Default to outputting PDF.
+(setq-default TeX-PDF-mode t)
 (setq auctex-latexmk-inherit-TeX-PDF-mode t)
+
+;; Completions for Bibtex.
+(require 'company-bibtex)
+(add-to-list 'company-backends 'company-bibtex)
+
+(let ((bibtex-path
+       (concat (getenv "HOME")
+               "/library.bib")))
+  (setq company-bibtex-bibliography bibtex-path))
