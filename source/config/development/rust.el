1b63148f9dd627b774fb914d6439a6fb0b84570d
Yghor Kerscher <kerscher@acm.org>
2017-02-26 19:39:52 +0100

    Rust packages through `use-package`.

--- source/config/development/rust.el
+++ source/config/development/rust.el
@@ -1,15 +1,21 @@
 ;; Rust
 
 ;; IDE support binary
-(setq racer-cmd "/home/yghor/.cargo/bin/racer")
-(setq racer-rust-src-path "/home/yghor/development/remote/rust/src/")
-
-;; Hooks
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-(add-hook 'rust-mode-hook  #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
-(add-hook 'rust-mode-hook
-          '(lambda ()
-             (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-             (local-set-key (kbd "TAB") #'company-indent-or-complete-common)))
+(use-package rust-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+  :config
+  (use-package racer
+    :ensure t
+    :init
+    (setq racer-cmd "/home/yghor/.cargo/bin/racer")
+    (setq racer-rust-src-path "/home/yghor/development/remote/rust/src/")
+    (add-hook 'rust-mode-hook  #'racer-mode)
+    (add-hook 'racer-mode-hook #'eldoc-mode)
+    (add-hook 'racer-mode-hook #'company-mode))
+  (add-hook 'rust-mode-hook
+            '(lambda ()
+               (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+               (local-set-key (kbd "TAB") #'company-indent-or-complete-common))))
+ 
