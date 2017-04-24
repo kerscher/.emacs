2a7de850b65649734c19e0ac4e0616ee6e5278c4
Yghor Kerscher <kerscher@acm.org>
2017-04-24 20:38:02 +0200

    Add Ansible, Dockerfile and NGINX support.

--- source/config/development/site-reliability-engineering.el
+++ source/config/development/site-reliability-engineering.el
@@ -1,4 +1,4 @@
-;;
+;; SRE
 
 (use-package hcl-mode :ensure t)
 
@@ -6,3 +6,15 @@
   :ensure t
   :init
   (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
+
+(use-package dockerfile-mode :ensure t)
+
+(use-package ansible
+  :ensure t
+  :config
+  (use-package ansible-doc
+    :ensure t
+    :init
+    (add-hook 'yaml-mode-hook #'ansible-doc-mode)))
+
+(use-package nginx-mode :ensure t)
