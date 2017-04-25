095a56c9262c20c0974b459e13a64242854d8210
Yghor Kerscher <kerscher@acm.org>
2017-04-25 16:00:04 +0200

    Allow `goenv` to be used with `go-mode`.

--- source/config/development/go.el
+++ source/config/development/go.el
@@ -2,6 +2,19 @@
 
 (use-package go-mode
   :ensure t
+  :init
+  (setenv "GOPATH"     (concat (getenv "HOME") "/go"))
+  (setenv "GOENV_ROOT" (concat (getenv "HOME") "/.goenv"))
+  (setenv "GOENV_PATH" (concat (getenv "GOENV_ROOT") "/bin"))
+  (setenv "PATH"       (concat
+                        (getenv "GOENV_ROOT") "/shims:"
+                        (getenv "GOENV_PATH") ":"
+                        (getenv "PATH")))
+  (setenv "GOROOT"
+          (replace-regexp-in-string
+           "\n"
+           ""
+           (shell-command-to-string "goenv prefix")))
   :config
   (use-package company-go :ensure t)
   (use-package go-errcheck :ensure t)
@@ -9,3 +22,5 @@
     :ensure t
     :init
     (add-hook 'go-mode-hook 'go-eldoc-setup)))
+
+
