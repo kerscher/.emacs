11bf8d545ae906b91d714d47ab1c3bebfe337f95
Yghor Kerscher <kerscher@outlook.com>
2018-05-05 13:51:55 +0200

    Increase `company-minimum-prefix-length`
    Using `0` was triggering too early. Going back to `3` might be even
    better. This should be tested out for a while.

--- source/config/development/common.el
+++ source/config/development/common.el
@@ -19,7 +19,7 @@
   (add-hook 'after-init-hook 'global-company-mode)
   (setq company-tooltip-align-annotations t)
   (setq company-idle-delay 0.1)
-  (setq company-minimum-prefix-length 0)
+  (setq company-minimum-prefix-length 1)
   (setq company-tooltip-limit 30)
   (use-package company-quickhelp
     :ensure t
