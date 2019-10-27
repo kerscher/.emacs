0139259da397334b98787a491db6e25a62722a01
Yghor Kerscher <kerscher@acm.org>
2019-10-27 13:01:26 +0000

    fix(go): GO* env variables should be set from calling env

--- source/config/development/go.el
+++ source/config/development/go.el
@@ -5,20 +5,6 @@
   (interactive)
   (use-package go-mode
     :ensure t
-    :init
-    (setenv "GO111MODULE" "on")
-    (setenv "GOPATH"     (concat (getenv "HOME") "/go"))
-    (setenv "GOENV_ROOT" (concat (getenv "HOME") "/.goenv"))
-    (setenv "GOENV_PATH" (concat (getenv "GOENV_ROOT") "/bin"))
-    (setenv "PATH"       (concat
-                          (getenv "GOENV_ROOT") "/shims:"
-                          (getenv "GOENV_PATH") ":"
-                          (getenv "PATH")))
-    (setenv "GOROOT"
-            (replace-regexp-in-string
-             "\n"
-             ""
-             (shell-command-to-string "goenv prefix")))
     :config
     (add-hook 'before-save-hook #'gofmt-before-save)
     (use-package flycheck-golangci-lint
