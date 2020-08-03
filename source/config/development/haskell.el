9c7f98bc0c81f0e2b36bf7dad3b27a1dddb0e9d1
Yghor Kerscher <kerscher@acm.org>
2020-08-03 16:43:01 +0100

    feat(haskell): use `lsp-haskell` instead of `intero`

--- source/config/development/haskell.el
+++ source/config/development/haskell.el
@@ -10,20 +10,12 @@
                 :map haskell-cabal-mode-map
                 ("C-c C-c" . haskell-compile))
     :init
+    (use-package lsp-haskell
+      :ensure t
+      :init
+      (add-hook 'haskell-mode-hook #'lsp))
     (setq haskell-compile-cabal-build-command "stack build")
     (speedbar-add-supported-extension ".hs")
     :config
-    (use-package hasky-stack :ensure t)
-    (use-package intero
-      :ensure t
-      :init
-      (require 'intero)
-      ;(add-hook 'haskell-mode-hook 'intero-mode)
-      :config
-      (defun kerscher/hpack-intero-restart ()
-        (interactive)
-        (shell-command
-         (format (concat "hpack " (intero-project-root))))
-        (intero-restart)))
     (eval-after-load "which-func"
       '(add-to-list 'which-func-modes 'haskell-mode))))
