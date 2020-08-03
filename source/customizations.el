2873107eb7a3a52f511a2dd88b02bd35a75349b4
Yghor Kerscher <kerscher@acm.org>
2020-08-03 16:42:06 +0100

    refactor(ansible): milder `ansible` highlighting

--- source/customizations.el
+++ source/customizations.el
@@ -55,6 +55,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansible-section-face ((t nil)))
+ '(ansible-task-label-face ((t nil)))
  '(rg-error-face ((t (:inherit compilation-error))))
  '(rg-info-face ((t (:foreground "white"))))
  '(rg-match-face ((t (:background "white smoke" :foreground "black")))))
