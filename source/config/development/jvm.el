4695e350bcf874b23eac213f57dd1fd1516804f5
Yghor Kerscher <kerscher@acm.org>
2020-08-03 16:45:20 +0100

    feat(java): `lsp-mode` (experimental)

--- source/config/development/jvm.el
+++ source/config/development/jvm.el
@@ -3,14 +3,9 @@
 (defun kerscher/lang/jvm ()
   "Load JVM-hosted languages"
   (interactive)
-  (use-package sbt-mode    :ensure t :pin melpa)
-  (use-package scala-mode  :ensure t :pin melpa)
-  (use-package ensime
+  (use-package groovy-mode :ensure t)
+  (use-package lsp-java
     :ensure t
-    :pin melpa
-    :init
-    (setq ensime-eldoc-hints 'all)
-    (setq ensime-search-interface 'helm)
-    (use-package flycheck-ensime :ensure t :pin melpa)
-    )
-  (use-package groovy-mode :ensure t :pin melpa))
+    :after lsp
+    :config
+    (add-hook 'java-mode-hook 'lsp)))
