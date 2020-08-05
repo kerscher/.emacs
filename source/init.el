0fc2df85dfdcb088c854501467f5ed6474128690
Yghor Kerscher <kerscher@acm.org>
2020-08-05 15:58:33 +0100

    feat(dhall): load `dhall` functionality on startup
    But only startup modes loading if called with
    `kerscher/lang/dhall`. This is so `local-environment.el` can decide to
    run it or not for faster startups.

--- source/init.el
+++ source/init.el
@@ -36,6 +36,7 @@
     "development/python"
     "development/rust"
     "development/site-reliability-engineering"
+    "development/dhall"
     "communication/email"
     "communication/irc"
     "communication/presentations"
