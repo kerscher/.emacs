4a478f4b11aa5fecab45d9527cbfe894bcea5d4a
Yghor Kerscher <kerscher@acm.org>
2021-03-15 16:33:48 +0000

    feat(lisp): use `geiser` for Scheme

--- source/config/development/lisp.el
+++ source/config/development/lisp.el
@@ -2,3 +2,8 @@
 
 ;; Always debug errors
 ;(customize-set-variable 'debug-on-error t)
+
+(defun kerscher/lang/lisp ()
+  "Load Lisp-related modes and features"
+  (interactive)
+  (use-package geiser :ensure t))
