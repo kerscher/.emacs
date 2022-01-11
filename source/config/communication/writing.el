2b02eb0ba1c856463bda1601021b2566c65720d7
Yghor Kerscher <yghor.kerscher@comcarde.com>
2022-01-11 14:37:20 +0000

    feat(writing): never `markdownfmt` (or `pandoc`) `CHANGELOG.md`
    This file is generated by `clog`, and formatting it messes it up badly.

--- source/config/communication/writing.el
+++ source/config/communication/writing.el
@@ -10,6 +10,15 @@
     (use-package markdownfmt
       :ensure t
       :init
-      (add-hook 'markdown-mode-hook #'markdownfmt-enable-on-save)
+      (add-hook 'markdown-mode-hook #'kerscher/markdown/markdownfmt-enable-on-save-except-changelogs)
       )
     (use-package markdown-preview-mode :ensure t)))
+
+(defun kerscher/markdown/markdownfmt-enable-on-save-except-changelogs ()
+  "Run markdown when saving buffer, except if it's a CHANGELOG file"
+  (interactive)
+  (add-hook 'before-save-hook
+            (lambda ()
+              (when (not (string= (file-name-nondirectory (buffer-file-name)) "CHANGELOG.md"))
+                (markdownfmt-format-buffer)))
+            nil t))
