0cceb69a6da67cdc0e2939300621af63690f6bde
Yghor Kerscher <yghor.kerscher@comcarde.com>
2022-07-11 11:58:27 +0100

    feat(navigation): remove window resizing shortcuts
    They were making selection by `next-word` and friends inconsistent.
    Use your mouse.

--- source/config/user-interface/navigation.el
+++ source/config/user-interface/navigation.el
@@ -20,11 +20,7 @@
 				("s-<down>" . windmove-down)
 				("s-<up>" . windmove-up)
 				("s-<left>" . windmove-left)
-				("s-<right>" . windmove-right)
-				("M-S-<down>" . shrink-window)
-				("M-S-<up>" . enlarge-window)
-				("M-S-<left>" . shrink-window-horizontally)
-				("M-S-<right>" . enlarge-window-horizontally)))
+				("s-<right>" . windmove-right)))
 
   (use-package zoom)
 
