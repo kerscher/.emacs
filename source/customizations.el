9829dd9fe3a04b98437bd64a98cda68689968cde
Yghor Kerscher <yghor.kerscher@comcarde.com>
2022-08-07 14:26:28 +0100

    chore: update customisations

--- source/customizations.el
+++ source/customizations.el
@@ -12,7 +12,7 @@
  '(company-quickhelp-delay 0.01)
  '(company-quickhelp-use-propertized-text t)
  '(custom-safe-themes
-   '("ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7" "cbd85ab34afb47003fa7f814a462c24affb1de81ebf172b78cb4e65186ba59d2" "8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c" "372e869d8ac016429c1ce08c931e659cc244d9750fc04094652c04f2706551de" "76046455ba22785c1013604593ac2b1d223a9fd615d22f7e9abc686c8e6478e5" "a17e1daefaff04a6faafe532ba7217fe9c83bd6c9ed6af52e2e439813b1762bf" default))
+   '("dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7" "cbd85ab34afb47003fa7f814a462c24affb1de81ebf172b78cb4e65186ba59d2" "8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c" "372e869d8ac016429c1ce08c931e659cc244d9750fc04094652c04f2706551de" "76046455ba22785c1013604593ac2b1d223a9fd615d22f7e9abc686c8e6478e5" "a17e1daefaff04a6faafe532ba7217fe9c83bd6c9ed6af52e2e439813b1762bf" default))
  '(erc-hide-list '("JOIN" "PART" "QUIT"))
  '(erc-nick "yyyyy")
  '(groovy-indent-offset 2)
@@ -33,15 +33,15 @@
  '(org-velocity-bucket "/var/shortcuts/notes/notes.org.gpg")
  '(package-archives
    '(("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.org/packages/")
-     ("org" . "http://orgmode.org/elpa/")))
+	 ("melpa" . "http://melpa.org/packages/")
+	 ("org" . "http://orgmode.org/elpa/")))
  '(package-selected-packages
    '(bats-mode yaml-mode scala-mode sbt-mode flycheck-golangci-lint ansible-vault caddyfile-mode flycheck-ensime rainbow rainbow-mode jinja2 php-mode helm-gtags company-lsp lsp-imenu projectile lsp-ui lsp-groovy lsp-python lsp-sh lsp-mode eglot blacken flycheck-mypy groovy-mode ensime restclient flycheck-mmark markdown-preview-mode markdownfmt markdown-mode+ haskell-mode anaconda-mode terraform-mode hcl-mode rust-mode go-mode flycheck-gometalinter color-theme-modern late-night late-night-theme zone-nyan yasnippet which-key wgrep vagrant-tramp vagrant use-package toml-mode systemd switch-window sr-speedbar spaceline salt-mode rg restclient-helm rebox2 rainbow-delimiters racer pyenv-mode purple-haze-theme org-plus-contrib org-bullets org-beautify-theme nyan-mode nix-mode nginx-mode mustache-mode mustache multiple-cursors monokai-alt-theme markdown-mode magit less-css-mode ledger-mode keychain-environment jq-mode jinja2-mode intero info+ helm-unicode helm-pass helm-mu helm-ls-git helm-bibtex hasky-stack god-mode go-errcheck go-eldoc github-modern-theme fold-this flycheck-yamllint flycheck-rust flycheck-ledger exotica-theme exec-path-from-shell epresent elm-mode eink-theme dockerfile-mode diminish company-terraform company-quickhelp company-go company-bibtex company-ansible company-anaconda cargo basic-theme auctex-latexmk ansible-doc ansible ace-jump-mode 0blayout))
  '(scroll-bar-mode nil)
  '(speedbar-show-unknown-files t)
  '(speedbar-use-images nil)
  '(speedbar-verbosity-level 0)
- '(sr-speedbar-auto-refresh nil t)
+ '(sr-speedbar-auto-refresh nil)
  '(sublimity-mode t t)
  '(warning-suppress-log-types '((comp))))
 (custom-set-faces
