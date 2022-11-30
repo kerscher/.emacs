d70a9975789cf72c9e40843183a31b183a91f3c5
Yghor Kerscher <kerscher@acm.org>
2022-11-30 11:49:14 +0000

    wip(agenda): `org-ref` and `org-roam` tweaks

--- source/config/agenda.el
+++ source/config/agenda.el
@@ -15,9 +15,25 @@
   (setq org-default-notes-file "/opt/src/notes.org.gpg")
   (setq org-agenda-files '("/opt/src/notes.org.gpg")))
 
+(use-package org-ref)
+
 (use-package org-roam
   :bind (("C-c n l" . org-roam-buffer-toggle)
 		 ("C-c n f" . org-roam-node-find)
 		 ("C-c n i" . org-roam-node-insert))
   :config
-  (org-roam-setup))
+  (org-roam-setup)
+  ;; (setq org-roam-capture-templates '(("d" "default" plain "%?"
+  ;; 									  :target (file+head "${slug}.org.gpg"
+  ;; 														 "#+title: ${title}\n")
+  ;; 									  :unnarrowed t)))
+  (add-to-list 'display-buffer-alist
+               '("\\*org-roam\\*"
+				 (display-buffer-in-side-window)
+				 (side . left)
+				 (slot . 0)
+				 (window-width . 0.33)
+				 (window-parameters . ((no-other-window . t)
+                                       (no-delete-other-windows . t))))))
+
+
